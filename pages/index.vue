d24289c36df5f0fc5730007a7261ecaa745a6d92
ChandraRaditya <chandra@refactory.id>
2024-01-22 23:53:06 +0700

    add tailwind

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div class="bg-red-500 w-50 h-72">hello this is main page</div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
